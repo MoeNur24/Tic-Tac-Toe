636fc434cb5c89d44dda15dc7efc0b9cd2708289
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-12-07 12:04:54 -0800

    Update JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ let currentPlayer = 'X';
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameActive = true;
 
+function handleCellClick(index) {
+    if (!gameActive || gameBoard[index] !== '') {
+        return;
+    }
+
+}
